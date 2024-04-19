3bd979bfde8bf2affd470a97140dc40443826638
Johan Coetzer <johan.coetzer@tsolo.io>
2024-04-19 20:08:52 +0200

    AT-2020 a bit of ska-ser-sphinx-theme left

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -58,7 +58,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["ska-ser-sphinx-templates/_templates"]
+# templates_path = ["ska-ser-sphinx-templates/_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -101,7 +101,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "ska_ser_sphinx_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
