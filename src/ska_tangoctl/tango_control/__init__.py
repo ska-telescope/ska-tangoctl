ced45d73acf83ac2cca80c8a71225f844d52bd09
Johan Coetzer <johan.coetzer@tsolo.io>
2024-04-25 11:37:52 +0200

    AT-2031 add API to documentation, with linting

--- src/ska_tangoctl/tango_control/__init__.py
+++ src/ska_tangoctl/tango_control/__init__.py
@@ -10,15 +10,9 @@ __all__ = [
     "TestTangoDevice",
 ]
 
+from ska_tangoctl.tango_control.read_tango_device import TangoctlDevice, TangoctlDeviceBasic
+from ska_tangoctl.tango_control.read_tango_devices import TangoctlDevices, TangoctlDevicesBasic
 from ska_tangoctl.tango_control.tango_control import TangoControl
 from ska_tangoctl.tango_control.tango_json import TangoJsonReader
-from ska_tangoctl.tango_control.read_tango_device import (
-    TangoctlDeviceBasic,
-    TangoctlDevice,
-)
-from ska_tangoctl.tango_control.read_tango_devices import (
-    TangoctlDevicesBasic,
-    TangoctlDevices,
-)
-from ska_tangoctl.tango_control.test_tango_script import TangoScript
 from ska_tangoctl.tango_control.test_tango_device import TestTangoDevice
+from ska_tangoctl.tango_control.test_tango_script import TangoScript
