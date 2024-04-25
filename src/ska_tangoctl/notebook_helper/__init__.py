85eb70d3b6cb8e16fe75706a7e6bdb47d6274030
Johan Coetzer <johan.coetzer@tsolo.io>
2024-04-25 13:08:45 +0200

    AT-2031 fix the linting

--- src/ska_tangoctl/notebook_helper/__init__.py
+++ src/ska_tangoctl/notebook_helper/__init__.py
@@ -1,20 +1,27 @@
 """Read information from Tango database."""
 
+import tango  # noqa: F401
+
+# TODO fix the problem and uncomment the functions below
+# They break the docs builder like this
+# docstring of ska_tangoctl.notebook_helper.get_tango_notebook.show_device_commands:1:
+# WARNING: py:class reference target not found: tango._tango.DeviceProxy
+# The following are affected
+# "connect_device",
+# "check_device",
+# "device_state",
+# "setup_device",
+# "get_tango_admin",
+# "show_device_commands",
+# "show_attribute_value",
+# "show_device_attributes",
 __all__ = [
     "check_tango",
-    "connect_device",
-    "check_device",
-    "device_state",
-    "setup_device",
-    "get_tango_admin",
     "set_tango_admin",
     "show_device_state",
     "show_command_inputs",
-    "show_device_commands",
     "show_attribute_value_scalar",
     "show_attribute_value_spectrum",
-    "show_attribute_value",
-    "show_device_attributes",
     "show_device_query",
     "run_command",
     "show_device",
@@ -29,28 +36,27 @@ __all__ = [
     "show_long_running_commands",
 ]
 
-import tango  # noqa: F401
-
+# TODO put these back, see above
+# check_device,
+# connect_device,
+# device_state,
+# get_tango_admin,
+# setup_device,
+# show_attribute_value,
+# show_device_attributes,
+# show_device_commands,
 from ska_tangoctl.notebook_helper.get_tango_notebook import (
     check_command,
-    check_device,
     check_tango,
-    connect_device,
-    device_state,
     get_obs_state,
-    get_tango_admin,
     run_command,
     set_tango_admin,
-    setup_device,
-    show_attribute_value,
     show_attribute_value_scalar,
     show_attribute_value_spectrum,
     show_attributes,
     show_command_inputs,
     show_commands,
     show_device,
-    show_device_attributes,
-    show_device_commands,
     show_device_markdown,
     show_device_query,
     show_device_state,
